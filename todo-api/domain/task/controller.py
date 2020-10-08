98fa705e8616a12efd8a9b7da5353aaba9ee52dd
Ivan Aguiar <ivan.aguiar.almeida@ccc.ufcg.edu.br>
2020-10-07 23:37:29 -0300

    Environment variable engine - Dynaconf

--- todo-api/domain/task/controller.py
+++ todo-api/domain/task/controller.py
@@ -1,7 +1,8 @@
+from dynaconf import settings
 from flask_restful import Resource
 
 
 class TaskItem(Resource):
     @staticmethod
     def get():
-        return {'task': 'Hello, World!'}
+        return "Welcome to the %s App! You're using the %s Environment!" % (settings.APP_NAME, settings.ENV_NAME)
